44dca2b5f6a659bc8afd9edb93b4c01385462315
mahmoudnabil133 <hodanabil144@gmail.com>
2026-06-20 07:53:37 +0300

    chore(layout): update root layout and providers

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar/Navbar";
+import { AuthProvider } from "../context/AuthContext";
+import { WishlistProvider } from "../context/WishlistContext";
 
 export default function RootLayout() {
   return (
     <div className="dark min-h-screen bg-[#0b1220]">
-      <Navbar />
-      <main className="pt-[72px]">
-        <Outlet />
-      </main>
+      <AuthProvider>
+        <WishlistProvider>
+          <Navbar />
+          <main className="pt-[72px]">
+            <Outlet />
+          </main>
+        </WishlistProvider>
+      </AuthProvider>
     </div>
   );
 }
