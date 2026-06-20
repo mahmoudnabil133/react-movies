e3b498b00a6a0000e8ab9bce7a2a18854fd16206
mahmoudnabil133 <hodanabil144@gmail.com>
2026-06-20 07:53:37 +0300

    feat(config): update shared constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,7 @@
 export const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p";
+export const AUTH_BASE_URL = import.meta.env.VITE_AUTH_BASE_URL || "";
 
 export const POSTER_SIZE = "w500";
 export const BACKDROP_SIZE = "original";
